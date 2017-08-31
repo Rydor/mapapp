e7d19b78062bf49e39e7602ca6767d67c5eaabdb
Rydor <rydorothy@gmail.com>
2017-08-30 20:56:43 -0500

    mapapp

---
+++ map1.py
@@ -0,0 +1,34 @@
+import folium
+import pandas
+
+data = pandas.read_csv("Volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
+
+def color_producer(elevation):
+    if elevation <= 1000.0:
+        return "green"
+    elif  1000.0 < elevation <= 3000.0:
+        return "orange"
+    else:
+        return "red"
+
+map = folium.Map(location=[29.3, -98.6], zoom_start=6,tiles="Mapbox Bright")
+fgv = folium.FeatureGroup(name="Volcanoes")
+
+for lt, ln, el in zip(lat, lon, elev):
+    cl = color_producer(el)
+    fgv.add_child(folium.CircleMarker(location=[lt, ln], popup=str(el)+ "m",
+    color="grey", fill_color=cl, radius=6, fill_opacity=0.7))
+
+fgp = folium.FeatureGroup(name="Population")
+
+fgp.add_child(folium.GeoJson(data=open("world.json", 'r', encoding='utf-8-sig'),
+style_function=lambda x: {'fillColor': 'green' if x['properties']['POP2005'] < 10000000
+else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
+map.save("Map1.html")
